6ab5faf558bca28b596988b9478ebe72860debe5
sReez <step.reznikoff@yandex.ru>
2023-04-03 14:58:31 +0300

    refactor: вывести ошибки в log

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,26 +9,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class ErrorHandler {
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNull(final NotFoundException e) {
-        return Map.of("Film not found", e.getMessage());
+    public String handleNull(final NotFoundException e) {
+        log.warn("NotFoundException: " + e.getMessage());
+        return "Not found";
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidation(final ValidationException e) {
-        return Map.of("Validation exception", e.getMessage());
+    public String handleValidation(final ValidationException e) {
+        log.warn("ValidationException" + e.getMessage());
+        return "Bad request";
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleError(final RuntimeException e) {
-        return Map.of("Server error", e.getMessage());
+    public String handleError(final RuntimeException e) {
+        log.error("Server error: " + e.getMessage());
+        return "Server error";
     }
 }
