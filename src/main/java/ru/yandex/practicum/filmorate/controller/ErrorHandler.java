1d82b83118111628d6b601caccb1308ad2c370ea
sReez <step.reznikoff@yandex.ru>
2023-04-03 15:03:08 +0300

    fix: вывести ошибки в log

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -9,28 +9,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
+import java.util.Map;
+
 @RestControllerAdvice
 public class ErrorHandler {
     private final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNull(final NotFoundException e) {
+    public Map<String, String> handleNull(final NotFoundException e) {
         log.warn("NotFoundException: " + e.getMessage());
-        return "Not found";
+        return Map.of("Not found", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidation(final ValidationException e) {
+    public Map<String, String> handleValidation(final ValidationException e) {
         log.warn("ValidationException" + e.getMessage());
-        return "Bad request";
+        return Map.of("Validation exception: ", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleError(final RuntimeException e) {
+    public Map<String, String> handleError(final RuntimeException e) {
         log.error("Server error: " + e.getMessage());
-        return "Server error";
+        return Map.of("Server error", e.getMessage());
     }
 }
